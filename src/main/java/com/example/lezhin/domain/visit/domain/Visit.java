d20a5f071e1fc87b5cbab34d0d1259e1558da2fd
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-15 22:17:58 +0900

    entity설정

---
+++ src/main/java/com/example/lezhin/domain/visit/domain/Visit.java
@@ -0,0 +1,26 @@
+package com.example.lezhin.domain.visit.domain;
+
+import com.example.lezhin.domain.user.domain.User;
+import com.example.lezhin.domain.webToon.domain.WebToon;
+import com.example.lezhin.global.entity.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Visit extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "webToon_id")
+    private WebToon webToon;
+
+}
