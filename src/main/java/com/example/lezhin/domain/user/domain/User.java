d58379cb8d759a86eb39ef5fb40633b7afddd3e7
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-16 13:04:52 +0900

    영속성 전이 추가

--- src/main/java/com/example/lezhin/domain/user/domain/User.java
+++ src/main/java/com/example/lezhin/domain/user/domain/User.java
@@ -1,12 +1,18 @@
 package com.example.lezhin.domain.user.domain;
 
+import com.example.lezhin.domain.disLike.domain.DisLike;
+import com.example.lezhin.domain.like.domain.Like;
 import com.example.lezhin.domain.user.enums.Gender;
 import com.example.lezhin.domain.user.enums.Type;
+import com.example.lezhin.domain.visit.domain.Visit;
+import com.example.lezhin.domain.webToon.domain.WebToon;
 import com.example.lezhin.global.entity.BaseEntity;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
@@ -30,4 +36,15 @@ public class User extends BaseEntity {
     private Type type;
 
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<WebToon> webToon = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Like> like = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<DisLike> disLike = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Visit> visit = new ArrayList<>();
 }
